da9a9d10b7396f6cf69fc8353872261e19f8a026
Elivelton <eliveltongsoficial@gmail.com>
2023-07-26 23:36:49 -0300

    modified params identification to id

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::controller(CustomerController::class)->group(function() {
     Route::get('/customers', 'index')->name('customer.index');
     Route::post('/customers', 'store')->name('customer.store');
-    Route::get('/customers/{identification}', 'show')->name('customer.show');
-    Route::delete('/customers/{identification}', 'delete')->name('customer.delete');
-    Route::put('/customers/{identification}', 'update')->name('customer.update');
+    Route::get('/customers/{id}', 'show')->name('customer.show');
+    Route::delete('/customers/{id}', 'delete')->name('customer.delete');
+    Route::put('/customers/{id}', 'update')->name('customer.update');
 });
